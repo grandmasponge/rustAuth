dcc6b77f90599bacb608a1a65fe5fc4b7f66b3f8
grandmasponge <tobyr2006@gmail.com>
2023-08-03 14:04:43 +0100

    if u can belive it more error handeling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use axum::http::{header, HeaderMap};
 use axum::response::IntoResponse;
 use axum::{
@@ -12,7 +14,6 @@ use dotenv::dotenv;
 use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 use myent::users;
 use myent::users::Entity as m_user;
-use sea_orm::error::DbErr;
 use sea_orm::ActiveValue::{NotSet, Set};
 use sea_orm::{
     sea_query::tests_cfg::json, ActiveModelTrait, ColumnTrait, Database, DatabaseConnection,
@@ -44,14 +45,14 @@ async fn main() {
     dotenv().ok();
     let secret = std::env::var("SECRET").expect("failed to load env variable");
     //will arc at later stage
-    let state = MyState {
+    let state = Arc::new(MyState {
         //database connection in a state due to not wating to constantly connect to the database would be rather inefficent if constantly done
         db: Database::connect("mysql://root@localhost:3306/auth")
             .await
             .expect("failed to connect to the database"),
         token: secret,
         //look in .env for secret remeber to change the secret when implimenting this
-    };
+    });
     //adding routes to the app pretty simple stuffs
     let app = Router::new()
         .route("/register", post(register))
@@ -66,14 +67,14 @@ async fn main() {
 }
 //registering the user
 async fn register(
-    State(state): State<MyState>,
+    State(state): State<Arc<MyState>>,
     Json(payload): Json<User>,
 ) -> Result<impl IntoResponse, (StatusCode, Json<JsonValue>)> {
     let person = m_user::find()
         .filter(users::Column::Username.contains(&payload.username))
         .one(&state.db)
         .await
-        .map_err(|err| {
+        .map_err(|_| {
             (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 Json(json!({"status":"fail"})),
@@ -85,14 +86,17 @@ async fn register(
             StatusCode::NOT_ACCEPTABLE,
             Json(json!({"message":"user already exists"})),
         )),
-        Ok(None) => Ok({
+        Ok(None) => Err({
             let user = users::ActiveModel {
                 id: NotSet,
                 username: Set(payload.username),
                 password: Set(payload.password),
             };
             let _user = user.insert(&state.db).await.map_err(|_| {
-                (StatusCode::INTERNAL_SERVER_ERROR, Json(json!({"status":"failed to create user"})))
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json(json!({"status":"failed to create user"})),
+                )
             });
 
             (
@@ -110,17 +114,22 @@ async fn register(
 }
 //the only function in this project that is somewhat been error handled
 async fn login(
-    State(state): State<MyState>,
+    State(state): State<Arc<MyState>>,
     Json(payload): Json<User>,
-) -> Result<HeaderMap, (StatusCode, Json<JsonValue>)> {
+) -> Result<impl IntoResponse, (StatusCode, Json<JsonValue>)> {
     let user = m_user::find()
         .filter(users::Column::Username.eq(&payload.username))
         .one(&state.db)
         .await
-        .expect("failed to retrive from db");
+        .map_err(|_| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"status":"fail"})),
+            )
+        });
     //checking user info for logging in
     match user {
-        Some(cl) => {
+        Ok(Some(cl)) => {
             println!("{:?}", cl);
             if cl.password == payload.password {
                 let iat = Utc::now().timestamp() as usize;
@@ -159,7 +168,7 @@ async fn login(
                 ))
             }
         }
-        None => Err((
+        Ok(None) => Err((
             StatusCode::UNAUTHORIZED,
             Json(json!(
                 {
@@ -167,10 +176,11 @@ async fn login(
                 }
             )),
         )),
+        Err(_) => unreachable!(),
     }
 }
 //todo! but for now juss checks if there is a cookie.
-async fn auth(jar: CookieJar, State(state): State<MyState>) {
+async fn auth(jar: CookieJar, State(state): State<Arc<MyState>>) {
     if let Some(cookie) = jar.get("token") {
         let token = decode::<Claims>(
             cookie.value(),
